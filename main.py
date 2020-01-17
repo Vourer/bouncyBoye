4f327f2ef892a45058688bea0431f2fccce82ce3
Adam Marciniak <51489464+Vourer@users.noreply.github.com>
2020-01-17 19:40:19 +0100

    Usprawnienie funkcji aktualizującej piłkę
    - nowe aktualizacje położenia i prędkości piłki poprzez algorytm skokowy
    - przeliczanie kątów na radiany, żeby prędkość początkowa miała jakiś sens
    - poprawki w komentarzach

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 import numpy as np
+from math import radians
 
 g = -9.81
 
@@ -9,25 +10,33 @@ class Pilka:
     # na razie "działa" część od rzutu pionowego
     # może niedługo  piłka będzie się poruszać w dwóch wymiarach, pod zadanym kątem początkowym
     def __init__(self, v0, h0, kat):
-        self.vx = 0.3                       # [m/s], prędkość pozioma //np.cos(kat%90) * v0
-        self.vy = np.sin(kat%90) * v0       # [m/s], prędkość pionowa
+        self.vx = 0.3                       # [m/s], prędkość pozioma //np.cos(radians(kat%90)) * v0
+        self.vy = np.sin(radians(kat%90)) * v0       # [m/s], prędkość pionowa
         self.x = 0                          # [m], położenie w poziomoie
         self.y = h0                         # [m], wysokość; równoznaczne z h, ale na wykresie lepsze jest y (chyba)
         self.hmax = self.vy**2 / 2*(-g)     # [m], maksymalna wysokość, na jaką może wznieść się piłka
 
 
 def aktualizuj_pilke(x_stare, y_stare, vx_stare, vy_stare, t, dt):
-    # uaktualnienie położenia x i y oraz prędkości vy - niech vx będzie na razie stałe
+    # uaktualnienie położenia x i y oraz prędkości vy stosując algorytm skokowy
+    # pierwsza część ruchu / pierwsza połowa dt
+    x_pol = x_stare + vx_stare * 0.5*dt
+    y_pol = y_stare + vy_stare * 0.5*dt
+
+    # aktualizacja prędkości
     vx_nowe = vx_stare #+ g * dt
     vy_nowe = vy_stare + g * dt
-    x_nowe = x_stare + vx_stare * dt
-    y_nowe = y_stare + vy_stare * dt
+
+    # druga część ruchu / druga połowa dt
+    x_nowe = x_pol + vx_nowe * 0.5*dt
+    y_nowe = y_pol + vy_nowe * 0.5*dt
 
     # sprawdzanie czy piłka nie no-clip'uje przez podłogę
     # jeśli tak, to robimy krok czasowy trochę mniejszsy, żeby akurat trafić w położenie y=0
     if y_nowe < 0:
         y_nowe = 0
-        dt_hop = -y_stare / vy_stare  # liczenie czasu dt, po którym nastąpi odbicie od podłogi
+        # poniżej liczenie czasu dt, po którym nastąpi odbicie od podłogi
+        dt_hop = (vy_stare + np.sqrt(vy_stare**2 + 2*g*y_stare)) / g
         vy_nowe = -(vy_stare + g * dt_hop)
         x_nowe = x_stare + vx_stare * dt_hop
         t += dt_hop
@@ -38,19 +47,17 @@ def aktualizuj_pilke(x_stare, y_stare, vx_stare, vy_stare, t, dt):
 
 
 def main():
-    v0 = 5 #input("Podaj prędkość początkową wyrzuconej piłki:")
-    kat = 60 #input("Podaj kąt rzucenia piłki:")
-    h0 = 0 #input("Podaj wysokość początkową piłki:")
-    dt = 0.1 #input("Podaj czas, po którym wartości mają być aktualizowane: ")
+    v0 = 20     #input("Podaj prędkość początkową wyrzuconej piłki:")
+    kat = 60    #input("Podaj kąt rzucenia piłki:")
+    h0 = 0      #input("Podaj wysokość początkową piłki:")
+    dt = 0.1    #input("Podaj czas, po którym wartości mają być aktualizowane:")
+    tmax = 15   #input("Podaj maksymalny czas trwania symulacji:")
     pilka = Pilka(v0, h0, kat)
 
-
     t = 0
-    tmax = 10
-    t_list, y_list =[], []
+    t_list, y_list =[], []  # listy, do których po każdej iteracji będą zapisywane obecny czas i wysokość
 
     while t <= tmax:
-        #print(pilka.x, pilka.y, pilka.vx, pilka.vy)
         pilka.x, pilka.y, pilka.vx, pilka.vy, t = aktualizuj_pilke(pilka.x, pilka.y, pilka.vx, pilka.vy, t, dt)
         t_list.append(t)
         y_list.append(pilka.y)
@@ -58,11 +65,10 @@ def main():
     # wykres wysokości od czasu
     fig, ax = plt.subplots()
     ax.plot(t_list, y_list)
-    plt.title("Wykres zależności wysokości h od upływu czasu t")
+    plt.title("Wykres zależności wysokości piłki h od upływu czasu t")
     plt.xlabel("t [s]")
     plt.ylabel("h [m]")
     plt.show()
-
 
 
 if __name__ == "__main__":
